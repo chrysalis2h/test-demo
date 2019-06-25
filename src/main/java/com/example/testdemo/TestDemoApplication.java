36316ab6e7cfb4d3680e7b7f09c1fd601c30b770
chrysalis2h <chrysalis2h>
2019-06-25 17:59:37 +0800

    测试servlet和actuator

--- src/main/java/com/example/testdemo/TestDemoApplication.java
+++ src/main/java/com/example/testdemo/TestDemoApplication.java
@@ -2,8 +2,11 @@ package com.example.testdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@ServletComponentScan(basePackages = "com.example.testdemo.web.servlet")
 public class TestDemoApplication {
 
     public static void main(String[] args) {
