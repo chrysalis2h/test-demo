cf1729f2d001a99720352e8c156099d7c69ee644
chrysalis2h <iamcoke@163.com>
2019-11-20 09:21:17 +0800

    feat: redis
    增加redis相关测试类

---
+++ src/main/java/com/example/testdemo/utils/redis/HyperLogLog.java
@@ -0,0 +1,13 @@
+package com.example.testdemo.utils.redis;
+
+/**
+ *  * @ClassName: HyperLogLog
+ *  * @Description: https://www.jianshu.com/p/f008ae58336a
+ *  * @Author: HeJin
+ *  * @Date: 2019\11\18 0018 10:57
+ *  * @Version: v1.0 文件初始创建
+ *
+ */
+public class HyperLogLog {
+
+}
