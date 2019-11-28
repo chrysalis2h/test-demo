4058ba021e2788b55b9d3ef55cb9c434f420cbb8
chrysalis2h <iamcoke@163.com>
2019-11-28 10:08:21 +0800

    feat: websocket增加
    test

---
+++ src/main/java/com/example/testdemo/web/controller/WebSocketController.java
@@ -0,0 +1,29 @@
+package com.example.testdemo.web.controller;
+
+import com.example.testdemo.utils.WebSocketServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *  * @ClassName: WebSocketController
+ *  * @Description: WebSocketController
+ *  * @Author: HeJin
+ *  * @Date: 2019\11\28 0028 9:53
+ *  * @Version: v1.0 文件初始创建
+ */
+@RestController
+@RequestMapping("/websocket")
+public class WebSocketController {
+
+    @GetMapping("/push")
+    public String connectWebScoket(String sid, String message) {
+        try {
+            WebSocketServer.sendInfo(message, sid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "success";
+    }
+}
