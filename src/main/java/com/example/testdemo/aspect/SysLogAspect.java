9b9cf8f11bb487fe4e8c0d461b4c4498c9a6626b
chrysalis2h <iamcoke@163.com>
2019-12-20 17:16:32 +0800

    feat: 增加Hutool工具类，增加日志切面

---
+++ src/main/java/com/example/testdemo/aspect/SysLogAspect.java
@@ -0,0 +1,55 @@
+package com.example.testdemo.aspect;
+
+import cn.hutool.log.Log;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+/**
+ *  * @ClassName: SysLogAspect
+ *  * @Description: SysLogAspect
+ *  * @Author: HeJin
+ *  * @Date: 2019\12\20 0020 15:50
+ *  * @Version: v1.0 文件初始创建
+ */
+@Aspect
+@Component
+public class SysLogAspect extends SysLogBaseAspect {
+
+    private static final Log logger = Log.get();
+
+    @Pointcut("execution(public * com.example.testdemo.web.controller..*.*(..))")
+    public void webLog() {
+    }
+
+    @Before("webLog()")
+    @Override
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        super.doBefore(joinPoint);
+    }
+
+    @Around("webLog()")
+    @Override
+    public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        return super.doAround(joinPoint);
+    }
+
+    @After("webLog()")
+    @Override
+    public void doAfter(JoinPoint joinPoint) throws Throwable {
+        super.doAfter(joinPoint);
+    }
+
+    @AfterReturning(pointcut = "webLog()", returning = "ret")
+    @Override
+    public void doAfterReturning(Object ret) throws Throwable {
+        super.doAfterReturning(ret);
+    }
+
+    @AfterThrowing(pointcut = "webLog()", throwing = "e")
+    @Override
+    public void doAfterThrowing(JoinPoint joinPoint, Throwable e) throws Throwable {
+        super.doAfterThrowing(joinPoint, e);
+    }
+}
