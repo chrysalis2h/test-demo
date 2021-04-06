1220c6cdbcb48045f098ef112fecad5bf0b7e852
chrysalis2h <iamcoke@163.com>
2021-04-06 16:19:27 +0800

    🍪 增加leetcode demo

---
+++ src/main/java/com/cycle/testdemo/demo/ThreadDemo/ThreadOfAllStatus.java
@@ -0,0 +1,19 @@
+package com.cycle.testdemo.demo.ThreadDemo;
+
+public class ThreadOfAllStatus {
+
+    public static void main(String[] args) {
+        Thread t = new Thread(new MyTask());
+        t.setName("");
+        t.start();
+        System.out.println("main thread end");
+    }
+
+    public static class MyTask implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("start");
+            System.out.println("end");
+        }
+    }
+}
---
+++ src/main/java/com/cycle/testdemo/demo/fragments/LeetcodeDemo.java
@@ -0,0 +1,36 @@
+package com.cycle.testdemo.demo.fragments;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName: LeetcodeDemo
+ * @Description: LeetcodeDemo
+ * @Author: HeJin
+ * @Date: 2021\4\6 0006 13:24
+ * @Version: v1.0 文件初始创建
+ */
+public class LeetcodeDemo {
+
+    /*
+     * @lc app=leetcode.cn id=242 lang=java
+     *
+     * [242] 有效的字母异位词
+     */
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] sArr = s.toCharArray();
+        for (int i = 0; i < sArr.length; i++) {
+            int index = t.indexOf(sArr[i]);
+            if (index < 0) {
+                return false;
+            }
+            t = t.substring(0, index) + t.substring(index + 1);
+        }
+        return t.length() == 0;
+    }
+
+
+}
